009175eed7c49ebeb00c15db559a5d391a5fe22d
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-10-03 12:24:40 +0200

    Started writing JoinReorderingTest

--- src/test/optimizer/strategy/join_reordering_rule_test.cpp
+++ src/test/optimizer/strategy/join_reordering_rule_test.cpp
@@ -1,10 +1,18 @@
 #include "base_test.hpp"
 
+#include "optimizer/abstract_syntax_tree/predicate_node.hpp"
+#include "optimizer/abstract_syntax_tree/join_node.hpp"
+#include "optimizer/abstract_syntax_tree/stored_table_node.hpp"
+#include "optimizer/strategy/join_reordering_rule.hpp"
+#include "storage/storage_manager.hpp"
+
 namespace opossum {
 
 class JoinReorderingRuleTest : public BaseTest {
- public:
+ protected:
   void SetUp() override {
+    StorageManager::get().add_table("table_a", load_table("src/test/tables/int.tbl", 0));
+    StorageManager::get().add_table("table_b", load_table("src/test/tables/int_int.tbl", 0));
 
   }
 
@@ -14,6 +22,20 @@ class JoinReorderingRuleTest : public BaseTest {
 };
 
 TEST_F(JoinReorderingRuleTest, SearchJoinGraph) {
+  /**
+   *      --Join--
+   *    /         \
+   *  table_a   table_B
+   */
+  auto plan_a = std::make_shared<JoinNode>(JoinMode::Inner, std::make_pair(ColumnID{0}, ColumnID{2}), ScanType::OpEquals);
+  plan_a->set_left_child(std::make_shared<StoredTableNode>("table_a"));
+  plan_a->set_right_child(std::make_shared<StoredTableNode>("table_b"));
+
+  std::vector<std::shared_ptr<AbstractASTNode>> vertices;
+  std::vector<JoinEdge> edges;
+
+  JoinReorderingRule::_search_join_graph(plan_a, vertices, edges, ColumnID{0});
+
 
 }
 
