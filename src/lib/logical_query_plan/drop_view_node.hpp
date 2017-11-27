8b64c6bad64f75f51d6a9e6318637da06a5a372c
Markus Dreseler <markus.dreseler@hpi.de>
2017-11-27 13:00:51 +0100

    temp

---
+++ src/lib/logical_query_plan/drop_view_node.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "abstract_lqp_node.hpp"
+
+namespace opossum {
+
+/**
+ * Node type to represent deleting a view from the StorageManager
+ */
+class DropViewNode : public AbstractLQPNode {
+ public:
+  explicit DropViewNode(const std::string& view_name);
+
+
+  std::string description() const override;
+  bool subtree_is_read_only() const override;
+
+  const std::string& view_name() const;
+
+ protected:
+  std::shared_ptr<AbstractLQPNode> _clone_impl() const override;
+  const std::string _view_name;
+};
+
+}  // namespace opossum
